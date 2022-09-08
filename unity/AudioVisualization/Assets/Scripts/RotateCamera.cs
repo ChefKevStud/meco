19898d48171f13446e6170a59a4195f57207eda5
Kevin <kevin.zgraggen@students.fhnw.ch>
2022-09-08 21:44:14 +0200

    improved movement

--- unity/AudioVisualization/Assets/Scripts/RotateCamera.cs
+++ unity/AudioVisualization/Assets/Scripts/RotateCamera.cs
@@ -4,32 +4,40 @@ public class RotateCamera : MonoBehaviour
 {
 
     public float speed = 1;
+    private Vector3 rotationDirection = new Vector3();
+    
     public float yPositionOffset = 2;
 
     void Start()
     {
         Vector3 startPosition = transform.position;
         transform.position = new Vector3(startPosition.x, startPosition.y + yPositionOffset, startPosition.z);
+        
+        rotationDirection = new Vector3(0, speed * Time.deltaTime, 0);
+        
     }
     
     void Update()
     {
 
+        if (Input.GetKey("right"))
+        {
+            rotationDirection = new Vector3(0, -speed * Time.deltaTime, 0);
+        }
         if (Input.GetKey("left"))
         {
-            transform.Rotate(0, -speed * Time.deltaTime, 0);
-        }
-        else
-        {
-            transform.Rotate(0, speed * Time.deltaTime, 0);
-        }
-        if (Input.GetKey("up"))
-        {
-            transform.Rotate(0, 0, speed * Time.deltaTime);
+            rotationDirection = new Vector3(0, speed * Time.deltaTime, 0);
         }
         if (Input.GetKey("down"))
         {
-            transform.Rotate(0, 0, -speed * Time.deltaTime);
+            rotationDirection = new Vector3(0, 0, speed * Time.deltaTime);
         }
+        if (Input.GetKey("up"))
+        {
+            rotationDirection = new Vector3(0, 0, -speed * Time.deltaTime);
+        }
+        
+        transform.Rotate(rotationDirection);
+        
     }
 }
