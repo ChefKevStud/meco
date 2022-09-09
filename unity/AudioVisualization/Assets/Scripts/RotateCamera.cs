5152810c8d78b4cdc3627dc59eb1c50568259987
Kevin <kevin.zgraggen@students.fhnw.ch>
2022-09-09 20:06:45 +0200

    improved camera movement options

--- unity/AudioVisualization/Assets/Scripts/RotateCamera.cs
+++ unity/AudioVisualization/Assets/Scripts/RotateCamera.cs
@@ -3,10 +3,17 @@
 public class RotateCamera : MonoBehaviour
 {
 
-    public float speed = 1;
+    private float speed = 4;
     private Vector3 rotationDirection = new Vector3();
     
     public float yPositionOffset = 2;
+
+    public GameObject mainCamera;
+
+    private float lastMousePosY;
+    private float mousePosDeltaY;
+    private float lastMousePosX;
+    private float mousePosDeltaX;
 
     void Start()
     {
@@ -14,11 +21,24 @@ public class RotateCamera : MonoBehaviour
         transform.position = new Vector3(startPosition.x, startPosition.y + yPositionOffset, startPosition.z);
         
         rotationDirection = new Vector3(0, speed * Time.deltaTime, 0);
-        
+
+        mainCamera = this.transform.Find("Main Camera").gameObject;
+
     }
     
     void Update()
     {
+
+        if (Input.GetKey("f") && speed < 64)
+        {
+            speed += 2;
+            rotationDirection = new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("s") && speed > 0)
+        {
+            speed -= 2;
+            rotationDirection = new Vector3(0, speed * Time.deltaTime, 0);
+        }
 
         if (Input.GetKey("right"))
         {
@@ -36,7 +56,31 @@ public class RotateCamera : MonoBehaviour
         {
             rotationDirection = new Vector3(0, 0, -speed * Time.deltaTime);
         }
-        
+        if (Input.GetKey("space"))
+        {
+            rotationDirection = new Vector3();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            mousePosDeltaX = lastMousePosX - Input.mousePosition.x;
+            mousePosDeltaY = lastMousePosY - Input.mousePosition.y;
+            
+            //transform.Rotate(0, -mousePosDeltaX * Time.deltaTime, 0);
+            //transform.Rotate(0, 0, -mousePosDeltaY * Time.deltaTime);
+
+            rotationDirection = new Vector3(0, -mousePosDeltaX * Time.deltaTime, 0);
+            
+            lastMousePosX = Input.mousePosition.x;
+            lastMousePosY = Input.mousePosition.y;
+        }
+        else
+        {
+            lastMousePosX = 0;
+            lastMousePosY = 0;
+        }
+
+        mainCamera.transform.Translate(0, 0, Input.mouseScrollDelta.y);
+
         transform.Rotate(rotationDirection);
         
     }
